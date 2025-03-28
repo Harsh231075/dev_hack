7ea74df4c5826c5236f36c4b259e987e042d027e
Harsh231075 <harshsingh231075@gmail.com>
2025-03-28 15:11:32 +0530

    feedback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Feedback from './components/FeedbackForm'; // Ensure the file path is cor
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Share from './pages/Share';
+import Main from './Home/Main';
 function App() {
   return (
     <Routes>
+      <Route path='/' element={<Main />} />
       <Route path="/" element={<Dashboard />} />
-      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/feedback/:eventId" element={<Feedback />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/share/:eventId' element={<Share />} />
